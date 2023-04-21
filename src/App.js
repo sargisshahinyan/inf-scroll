83f25e88d55e4085009d3c829beb003423aefbcf
sargisshahinyan <shahinyan.sargis@gmail.com>
2023-04-21 09:48:29 +0200

    Added scrollable items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+
 import './App.scss';
 
+const randomString = () => {
+  return Math.random().toString(35).split('.')[1];
+};
+
 function App() {
+  const [items, setItems] = useState(() => Array(30).fill(null).map(() => randomString()));
+
   return (
     <div className="App">
-
+      {items.map((item) => (
+          <div key={item} className="item">
+            {item}
+          </div>
+      ))}
     </div>
   );
 }
